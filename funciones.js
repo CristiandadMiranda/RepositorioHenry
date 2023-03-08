0c833a3b7efe52cf6002f5f6aa679a58c0486917
Cristian Miranda <mirandadehoyos2003@gmail.com>
2023-03-08 09:11:35 -0500

    Archivosde prepCourse añadidos tipos de operadores y funciones aun sin añadir los de comparacion

---
+++ funciones.js
@@ -0,0 +1,39 @@
+// function sumaTres(x) {
+//     console.log(x + 5)
+// }
+
+// sumaTres(3)
+
+// //PRIMERA FORMA
+
+// function sumaTres1(x) {
+//     return x + 5;
+// }
+
+// //SEGUNDA FORMA
+
+// var sumaTres2 = function(x) {
+//     return x + 5;
+// }
+
+// //TERCERA FORMA
+
+// var sumaTres3 = (x) =>{
+//     return x + 5;
+// }
+
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+}
+
+function otraFuncion() {
+    return (`El nombre retornado por la funcion "cuidadoConElConsoleLog" es: 
+    ${cuidadoConElConsoleLog("Cristian")}`)
+}
+
+function cuidadoConElReturn() {
+    return nombre;
+    console.log(nombre)
+}
+
